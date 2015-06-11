f1ab4bf7ba115934354f9f76e3ed530d9e67f49d
jcollyer <collyerdesign@gmail.com>
2015-06-11 08:32:50 -0700

    add timer

--- app/assets/javascripts/components/components.jsx
+++ app/assets/javascripts/components/components.jsx
@@ -7,6 +7,7 @@ var setDementions = function(){
 }
 
 var userZones = [];
+var lineAnimationTime = 10000;
 
 var setUserZones = function() {
   var zones = document.getElementsByClassName("zone");
@@ -16,114 +17,81 @@ var setUserZones = function() {
   return userZones;
 };
 
-var HrZoneChart = React.createClass({
-  drawDataVisualization: function() {
-
-    setDementions();
-    setUserZones();
-
-    // Add an SVG element with the desired dimensions and margin.
-    var barGraph = d3.select("#barGraph").append("svg:svg")
-                     .attr("width", w + m[1] + m[3])
-                     .attr("height", h + m[0] + m[2])
-                     .append("svg:g")
-                     .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-    var xAxis = d3.scale.linear()
-                  .domain([0, d3.max(userZones)])
-                  .range([0, w]);
-
-    var yAxis = d3.scale.ordinal().domain(userZones).rangeBands([0, h]);
-
-    // var y = d3.scale.linear().domain([d3.min(userZones), d3.max(userZones)]).range([h, 0]);
-
-    barGraph.selectAll("rect")
-            .data(userZones)
-            .enter().append("rect")
-            .attr("y", yAxis)
-            .attr("width", xAxis)
-            .attr("height", yAxis.rangeBand())
-            .attr("fill", function(d) { return "rgba(0, 100, " + (d ) + ", 0.5)"});
-
-    barGraph.selectAll("text")
-            .data(userZones)
-            .enter().append("text")
-            .attr("x", xAxis)
-            .attr("y", function(d) { return yAxis(d) + yAxis.rangeBand() / 2; })
-            .attr("dx", -3) // padding-right
-            .attr("dy", ".35em") // vertical-align: middle
-            .attr("text-anchor", "end") // text-align: right
-            .text(String);
-
-
-    // // create left yAxis
-    // var yAxisLeft = d3.svg.axis().scale(yAxis).orient("left");
-
-    // barGraph.append("svg:g")
-    //      .attr("class", "y axis")
-    //      .attr("transform", "translate(-25,0)")
-    //      .call(yAxisLeft);
-
-  },
-  componentDidMount: function() {
-    this.drawDataVisualization();
-  },
-  render: function() {
-    return <div id="barGraph" className="aGraph"></div>;
+var CountDown = React.createClass({
+  render: function(){
+    return <div className="timer"></div>;
   }
 });
 
-
-
+// 250 * x = 10000
 
 var HrmSessionButton = React.createClass({
+
+  startCountDown: function(duration) {
+    that = this;
+    console.log(duration)
+    // debugger;
+    var timer = duration, minutes, seconds;
+    // var milliseconds = duration/10;
+    var milliseconds = 8.5;
+    var timeInterval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        that.setState({time: minutes + ":" + seconds});
+        // console.log(that.state.time);
+
+        if (--timer < 0) {
+            timer = 0;
+            clearInterval(timeInterval);
+        }
+    },milliseconds);
+  },
   drawDataVisualization: function() {
     that = this;
     var bpmData = [];
     var durationData = [];
+    var sessionDuration = that.props.session_duration;
+
+
 
     setDementions();
     setUserZones();
+    $("#lineGraph").html("");
 
     d3.json("http://localhost:3000/users/"+user_id+"/hrm_sessions/"+session_id+".json", function(data){
+      // hide loading overlay
       that.setState({loading:false});
+      //start count-down
+      that.startCountDown(sessionDuration);
+      //set data point object array
       data_points = data.hrm_session.hrm_data_points;
 
+      // make arrays of session bpm and duration
       data_points.map(function(data_point){
         bpmData.push(data_point.bpm);
         durationData.push(data_point.duration);
       });
-// debugger;
+
       // X scale will fit all values from data[] within pixels 0-w
       var x = d3.scale.linear().domain([0, bpmData.length]).range([0, w]);
 
       // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
       // automatically determining max range can work something like this
-      // var y = d3.scale.linear().domain(data_points).range([h, 0]);
       var y = d3.scale.linear().domain([d3.min(bpmData), d3.max(bpmData)]).range([h, 0]);
-
-
-
-
 
       // create a line function that can convert data[] into x and y points
       var line = d3.svg.line()
       // .interpolate("cardinal") //smooth out points
-      // assign the X function to plot our line as we wish
       .x(function(d,i) {
-        // debugger;
         return x(i);
       })
       .y(function(d) {
         return y(d);
       })
-
-      // .x(function(d) { return d.bpm; })
-      // .y(function(d) { return d.duration; });
-      // .interpolate("linear");
-
-
-
 
       // Add an SVG element with the desired dimensions and margin.
       var lineGraph = d3.select("#lineGraph").append("svg:svg")
@@ -153,29 +121,26 @@ var HrmSessionButton = React.createClass({
            .attr("x", w / 2)
            .attr("y", 50)
            .style("text-anchor", "middle")
-           .text(that.props.session_duration + " minutes" + " | " + data_points.length + " total sessions" );
-
+           .text(sessionDuration + " minutes" + " | " + data_points.length + " total sessions" );
 
       // Add the line by appending an svg:path element with the data line we created above
       // do this AFTER the axes above so that the line is above the tick-lines
-      var linePath = lineGraph.append("svg:path")
-                              .attr("d", line(bpmData));
+      var linePath = lineGraph.append("svg:path").attr("d", line(bpmData));
 
       var totalLength = linePath.node().getTotalLength();
-      // debugger;
 
       linePath.attr("stroke-dasharray", totalLength + " " + totalLength)
               .attr("stroke-dashoffset", totalLength)
               .transition()
-                .duration(2000)
+                .duration(sessionDuration * 10)
                 .ease("linear")
                 .attr("stroke-dashoffset", 0);
 
-
+                // 22 36:41
     });
   },
   getInitialState: function() {
-    return {hrm_data_points: [], total_time: "", loading: false};
+    return {hrm_data_points: [], total_time: "", loading: false, time: ""};
   },
   componentDidMount: function() {
     user_id = document.location.href.split('/').pop(-1);
@@ -207,8 +172,9 @@ var HrmSessionButton = React.createClass({
   render: function() {
     return (
       <div>
-      <div id="loader" className={this.state.loading? "is-loading" : ""}><h1>Loading...</h1></div>
-      <div id="lineGraph" className="aGraph"></div>
+        <div className="count-down">{this.state.time}</div>
+        <div id="loader" className={this.state.loading? "is-loading" : ""}><h1>Loading...</h1></div>
+        <div id="lineGraph" className="aGraph"></div>
         <button onClick={this.handleClick}>DataPonts</button>
       </div>
     );
